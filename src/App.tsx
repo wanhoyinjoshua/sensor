e3803de525c310e451aef89bba835304644eff50
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-05-25 15:13:12 +1000

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Sensor from './components/Sensor'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     
