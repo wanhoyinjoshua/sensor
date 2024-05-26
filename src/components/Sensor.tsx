1e191cb2118f6d3b95edcaf8b22c598921d32a8a
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-05-26 17:28:34 +1000

    hi

--- src/components/Sensor.tsx
+++ src/components/Sensor.tsx
@@ -137,6 +137,7 @@ const Sensor = (props:any) => {
    
   return (
     <div>
+        <br></br>
         <button  className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         onClick={()=>{setSensor(!sensor)}}>{sensor==true?<span>Hide</span>:<span>Show</span>} Calibrate Screen</button>
       
